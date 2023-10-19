ebcfadfcd20c460dfca45fe1558982d97e64cd7e
Ubuntu <ubuntu@ip-172-31-57-131.ec2.internal>
2023-10-19 11:04:08 +0000

    advance

--- movies/templates/tailwind.config.js
+++ movies/templates/tailwind.config.js
@@ -5,8 +5,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-    ],
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 }
-
